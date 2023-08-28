ca9523335c2e14c6e7eacdb893297498c79dde15
liyl <liyl02@superred.com.cn>
2023-08-28 14:24:49 +0800

    SpringCloud Eureka

---
+++ micro-service-cloud-eureka-7001/src/test/java/com/superred/demo/microservicecloudeureka7001/MicroServiceCloudEureka7001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superred.demo.microservicecloudeureka7001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceCloudEureka7001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
